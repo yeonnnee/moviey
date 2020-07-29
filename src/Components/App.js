fb17f8ad68b58206552addc0ffb3e881f3635d4e
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-07-30 01:45:23 +0900

    Create router

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import Router from "./Router";
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router />
   );
 }
 
---
+++ src/Components/Router.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+import Home from "../Routes/Home";
+import Movie from "../Routes/Movie";
+import TV from "../Routes/TV";
+import Search from "../Routes/Search";
+import Header from "../Components/Header";
+
+export default () => {
+  return (
+    <>
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movie" exact component={Movie} />
+          <Route path="/tv" exact component={TV} />
+          <Route path="/search" exact component={Search} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </Router>
+    </>
+  );
+};
