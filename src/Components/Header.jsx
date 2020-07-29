daf4056da04c51c781680405a192539e0778daec
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-07-30 01:44:51 +0900

    Create header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Link } from "react-router-dom";
+
+export default () => {
+  render()(
+    <header>
+      <ul>
+        <li>
+          <Link to="/">HOME</Link>
+        </li>
+        <li>
+          <Link to="/movie">MOVIE</Link>
+        </li>
+        <li>
+          <Link to="/tv">TV</Link>
+        </li>
+        <li>
+          <Link to="/search">SEARCH</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
