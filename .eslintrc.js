cf20671b5698303a8795b662456c1e1f6671d0cd
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-07-30 01:44:07 +0900

    Install package

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/jsx-filename-extension": falase,
+  },
+};
