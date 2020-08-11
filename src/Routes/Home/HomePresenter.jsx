697db96c243a5ef4ff99abe483d2bc1adb2dfe30
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-08-12 01:50:04 +0900

    Set styles for Home page

--- src/Routes/Home/HomePresenter.jsx
+++ src/Routes/Home/HomePresenter.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
+import Poster from "../../Components/Poster";
+
 const Container = styled.div`
-  padding-top: 60px;
+  padding: 70px 60px;
   color: white;
 `;
-const List = styled.ul``;
-const Item = styled.li``;
+const List = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  grid-auto-rows: 260px;
+  gap: 20px;
+  margin-bottom: 50px;
+`;
+
 const Title = styled.div`
   font-size: 20px;
   font-weight: bold;
-  margin-bottom: 10px;
+  margin-bottom: 20px;
+  margin-left: 20px;
 `;
 
 const HomePresenter = ({
@@ -24,28 +33,56 @@ const HomePresenter = ({
 }) =>
   isLoading ? null : (
     <Container>
+      <Title>Popular Movies</Title>
       <List>
-        <Title>Popular Movies</Title>
         {popularMovies.map((movie) => (
-          <Item key={movie.id}>{movie.title}</Item>
+          <Poster
+            key={movie.popularity}
+            id={movie.id}
+            title={movie.original_title}
+            imageUrl={movie.poster_path}
+            genres={movie.genre_ids}
+            isMovie={true}
+          />
         ))}
       </List>
+      <Title>Popular TV series</Title>
       <List>
-        <Title>Popular TV series</Title>
         {popularShows.map((show) => (
-          <Item key={show.id}>{show.name}</Item>
+          <Poster
+            key={show.popularity}
+            id={show.id}
+            title={show.original_name}
+            imageUrl={show.poster_path}
+            genre={show.genre_ids}
+            isMovie={false}
+          />
         ))}
       </List>
+      <Title>Comming Soon</Title>
       <List>
-        <Title>Comming Soon</Title>
         {upComingMovies.map((movie) => (
-          <Item key={movie.id}>{movie.title}</Item>
+          <Poster
+            key={movie.popularity}
+            id={movie.id}
+            title={movie.original_title}
+            imageUrl={movie.poster_path}
+            genre={movie.genre_ids}
+            isMovie={true}
+          />
         ))}
       </List>
+      <Title>Top Rated</Title>
       <List>
-        <Title>Top Rated</Title>
         {topRatedShows.map((show) => (
-          <Item key={show.id}>{show.name}</Item>
+          <Poster
+            key={show.popularity}
+            id={show.id}
+            title={show.original_name}
+            imageUrl={show.poster_path}
+            genre={show.genre_ids}
+            isMovie={false}
+          />
         ))}
       </List>
     </Container>
